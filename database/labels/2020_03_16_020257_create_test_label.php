09194e34cfa2ffb756d42bbdcce84fb52443020e
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2020-03-16 09:26:53 +0700

    edit name CreateTestLabel

--- database/labels/2020_03_16_020257_create_test_label.php
+++ database/labels/2020_03_16_020257_create_test_label.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Vinelab\NeoEloquent\Schema\Blueprint;
 use Vinelab\NeoEloquent\Migrations\Migration;
 
-class DummyClass extends Migration
+class CreateTestLabel extends Migration
 {
     /**
      * Run the migrations.
