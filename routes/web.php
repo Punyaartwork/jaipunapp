1a06dbfb3712f4b62e1d97e0999447b0ad4f32e5
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2020-03-16 09:57:47 +0700

    set test name

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     $user = App\Test::create(['name' => 'Tester', 'email' => 'some@email.com']);
     return view('welcome');
 });
-Route::get('set/{name}', function ($name)  {
+Route::get('/set/{name}', function ($name)  {
     $user = App\Test::create(['name' => $name, 'email' => 'some@email.com']);
     return view('welcome');
 });
