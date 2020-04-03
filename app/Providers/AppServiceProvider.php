d07528ad75275f0392a12ff73e76f5baabe5155b
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2020-04-03 11:15:08 +0700

    add in register

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app['request']->server->set('HTTPS', true);
+        $this->app['request']->server->set('HTTPS', $this->app->environment() != 'local');   
     }
 }
