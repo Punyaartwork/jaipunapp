0a5df74e133554c948d7362e8b72b47f91d8fc96
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2020-04-04 20:34:33 +0700

    edit api

--- routes/api.php
+++ routes/api.php
@@ -138,7 +138,7 @@ Route::get('stayinjoin/{id}', function($id) {
     $join = Join::find($id);
     //$stay = $join->stays;showjoin
     //return $join;
-    return  Stay::join($join)->get();
+    return  $join->stays()->edge();
     //return $stay->with('join')->with('user')->paginate(10);
 });
  
