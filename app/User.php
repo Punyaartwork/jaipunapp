48f8b530f76dc5e4dc493d334e482335307b1fd7
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2020-04-08 08:11:33 +0700

    edit User.php

--- app/User.php
+++ app/User.php
@@ -11,14 +11,14 @@ class User extends NeoEloquent
     protected $fillable = ['name','account','detail','profile', 'sex','api_token','fire_token'];
     public function joins()
     {
-        return $this->hasMany('App\Join', 'JOINED');
+        return $this->hasMany('App\Join', 'JOINED')->with('user')->latest();
     }
     public function posts()
     {
-        return $this->hasMany('App\Post', 'POSTED');
+        return $this->hasMany('App\Post', 'POSTED')->with('user')->latest();
     }
     public function stays()
     {
-        return $this->hasMany('App\Stay', 'STAYED');
+        return $this->hasMany('App\Stay', 'STAYED')->with('user')->latest();
     }
 }
