581640a7cecc348b7b7cbf985da6e35997be3d0f
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2020-04-17 14:36:19 +0700

    edit api

--- routes/api.php
+++ routes/api.php
@@ -260,11 +260,11 @@ Route::get('checkstayinjoin/{id}/{user_id}', function($id,$user_id) {
     //$stay = $join->stays;showjoin
     //return $join;
     $collection = collect($join->stays);
-    /*$result =$collection->filter(function ($value, $key) {
+    $result =$collection->filter(function ($value, $key) {
         return $value->user->id == 1;
-    });*/
-    if($collection->contains($user_id)){$result = 'true';}else{$result ='false';}
-    return  $user_id; 
+    });
+    //if($collection->contains($user_id)){$result = 'true';}else{$result ='false';}
+    return  $result; 
     //$result;
     //return $stay->with('join')->with('user')->paginate(10);
 });
