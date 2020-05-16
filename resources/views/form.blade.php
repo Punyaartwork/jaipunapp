02921003341b752387f6176bd5c093e733883b50
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2020-05-16 14:26:36 +0700

    edit api

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -379,9 +379,9 @@ $(document).ready(function(){
 
         var form = $(this);
         var url = "https://jaipungo.herokuapp.com/punsave/boon<?php echo $boonid ?>";
-        var url = form.attr("action");
+        //var url = form.attr("action");
         var formData = $(form).serializeArray();
-        $.post(url, new FormData(this)).done(function (data) {
+        $.post(url, formData).done(function (data) {
             alert(data);
         });
 
