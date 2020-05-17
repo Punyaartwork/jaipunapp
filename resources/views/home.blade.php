6e025c59b874919e17c8622cffc258c7d1ca103b
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2020-05-17 16:21:39 +0700

    change title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <link rel="shortcut icon" href="/jaipunround.ico" type="image/x-icon">     
-<title>JaiPun - Home page</title>
+<title>ตู้ปันบุญ - มาร่วมปันบุญ ช่วยโลกนี้ให้พ้นภัยโควิดด้วยกัน เป้าหมาย 10,000 การปัน</title>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 <style class="INLINE_PEN_STYLESHEET_ID">
     body {
