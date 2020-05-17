cc8363d45aff89deb82b563d82d3801c98a98dbf
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2020-05-17 14:24:07 +0700

    add check loading

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -404,8 +404,9 @@ z-index:-1;
     
 </div>
 <script src="https://static.codepen.io/assets/common/stopExecutionOnTimeout-157cd5b220a5c80d4ff8e0e70ac069bffd87a61252088146915e8726e5d9f147.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script id="INLINE_PEN_JS_ID">
-    
+        $(window).load(function(){alert('loading')});
     //# sourceURL=pen.js
   </script>
 
